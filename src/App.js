49080f1fe03f4108dc41bba4695feb00cd81aecd
NatsukiSh <108126196+NatsukiSh@users.noreply.github.com>
2023-01-10 23:01:57 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import logo from "./Signature.png";
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="My-logo img-fluid" alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+App;
