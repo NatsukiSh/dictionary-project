4fee924f093ca4e923421b0e9015c44784c8d8c9
NatsukiSh <108126196+NatsukiSh@users.noreply.github.com>
2023-01-10 23:34:36 +0100

    Added searching form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 import "./App.css";
 import logo from "./Signature.png";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="My-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="My-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            <a
+              href="https://github.com/NatsukiSh/dictionary-project"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Open-sourced
+            </a>
+            coded with ❤ by Natsuki Shuko and on{" "}
+            <a
+              href="https://app.netlify.com/sites/magical-kringle-3a7435/overview"
+              rel="noreferrer"
+              target="_blank"
+            >
+              Netlify
+            </a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
