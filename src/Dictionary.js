7f252717149a37ea10a417f3b45f081b196982ad
NatsukiSh <108126196+NatsukiSh@users.noreply.github.com>
2023-01-12 19:19:07 +0100

    Built Synonyms component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -4,8 +4,8 @@ import "./Dictionary.css";
 import Results from "./Results";
 
 export default function Dictionary() {
-  const [keyword, setKeyword] = useState(" ");
-  const [results, setResults] = useState(null);
+  let [keyword, setKeyword] = useState(" ");
+  let [results, setResults] = useState(null);
 
   function handleResponse(response) {
     console.log(response.data[0]);
