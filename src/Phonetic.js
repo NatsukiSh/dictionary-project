c9c8935c67a03a6f55a3efd5de578d7cd11d6328
NatsukiSh <108126196+NatsukiSh@users.noreply.github.com>
2023-01-19 16:24:28 +0100

    Replacing with another audio player

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,13 +1,39 @@
 import React from "react";
 import "./Phonetic.css";
-import ReactAudioPlayer from "react-audio-player";
+import AudioPlayer from "react-modern-audio-player";
 
 export default function Phonetic(props) {
-  return (
-    <div className="Phonetic">
-      <ReactAudioPlayer src={props.phonetic.audio} controls />
-
-      <span className="text">{props.phonetic.text}</span>
-    </div>
-  );
+  <div className="Phonetic">
+    return ( function App(){props.phonetic.audio}
+    {
+      <div>
+        <AudioPlayer
+          src={props.phonetic.audio}
+          audioInitialState={{
+            muted: true,
+            volume: 0.2,
+            curPlayId: 1,
+          }}
+          placement={{
+            interface: {
+              templateArea: {
+                trackTimeDuration: "row1-5",
+                progress: "row1-4",
+                playButton: "row1-6",
+                repeatType: "row1-7",
+                volume: "row1-8",
+              },
+            },
+            player: "bottom-left",
+          }}
+          activeUI={{
+            all: true,
+            progress: "waveform",
+          }}
+        />
+      </div>
+    }
+    );
+    <span className="text">{props.phonetic.text}</span>
+  </div>;
 }
